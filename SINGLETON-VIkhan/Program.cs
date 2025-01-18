33a5e5d93812847d74d78f752006d163aa16408d
vikhanmuhammad <muhammadvikhanmuharram@gmail.com>
2025-01-18 19:56:50 +0700

    Penambahan Singleton

---
+++ SINGLETON-VIkhan/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Net.NetworkInformation;
+
+public sealed class Singleton
+{
+    private static Singleton _instance;
+    private List<string> Data;
+    private Singleton() 
+    { 
+        Data = new List<string>();
+    }
+
+
+    public static Singleton GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new Singleton();
+        }
+        return _instance;
+    }
+
+    public List<string> GetData()
+    {
+        return Data;
+    }
+
+    public void PrintData()
+    {
+        for(int i = 0; i < Data.Count; i++)
+        {
+            Console.WriteLine(Data[i]);
+        }
+    }
+
+    public void AddData(string input)
+    {
+        Data.Add(input);
+    }
+
+    public void RemoveData(int index) 
+    {
+        Data.RemoveAt(index);
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Singleton s1 = Singleton.GetInstance();
+        Singleton s2 = Singleton.GetInstance();
+
+        s1.AddData("Yaris");
+        s1.AddData("Fortuner");
+        s1.AddData("Camry");
+        s1.AddData("Innova");
+        s1.AddData("Rush");
+
+        s2.PrintData();
+        Console.WriteLine("");
+
+        s2.RemoveData(2);
+        s1.PrintData();
+    }
+}
+
---
+++ teskpl-vikhan/SINGLETON.cs
@@ -0,0 +1,56 @@
+﻿namespace teskpl_vikhan
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Inisialisasi 2 objek singleton
+            DatabaseSingleton ds1 = DatabaseSingleton.GetInstance();
+            DatabaseSingleton ds2 = DatabaseSingleton.GetInstance();
+
+            Console.WriteLine("Mengisi data di ds1 dengan isi 'data awal'.");
+            ds1.updateData("data awal");
+
+            Console.Write("Berikut adalah isi ds1 : ");
+            ds1.printData();
+
+            Console.Write("Berikut adalah isi ds2 : ");
+            ds2.printData();
+
+            Console.WriteLine();
+            Console.WriteLine("Mengisi data di ds2 dengan isi 'data terbaru'.");
+            ds1.updateData("data terbaru");
+
+            Console.Write("Berikut adalah isi ds1 : ");
+            ds1.printData();
+
+            Console.Write("Berikut adalah isi ds2 : ");
+            ds2.printData();
+        }
+    }
+    public sealed class DatabaseSingleton
+    {
+        private DatabaseSingleton() { }
+
+        private static DatabaseSingleton _instance;
+        public static string data;
+
+        public static DatabaseSingleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new DatabaseSingleton();
+            }
+            return _instance;
+        }
+        public void updateData(string update)
+        {
+            data = update;
+        }
+
+        public void printData()
+        {
+            Console.WriteLine(data);
+        }
+    }
+}
